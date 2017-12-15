2c5c1426a9f13622054d28f4a5523cf6c7d2cda6
wpfhtl <wpfhtl@gmail.com>
2017-12-15 13:51:00 +0800

    read image sequences directly instead of using cfg file

--- ADCensusBM/main.cpp
+++ ADCensusBM/main.cpp
@@ -43,317 +43,92 @@
 
 #include <iostream>
 #include "stereoprocessor.h"
-#include <libconfig.h++>
 #include "imageprocessor.h"
 #include <cstdlib>
-#include <boost/filesystem.hpp>
-#include <boost/date_time.hpp>
-#include <iomanip>
-
-#include <pcl/common/common_headers.h>
-#include <pcl/io/pcd_io.h>
 
 using namespace std;
 using namespace cv;
 
-bool loadImageList(string file, vector<string> &list)
-{
-    bool loadSuccess = false;
-    FileNode fNode;
-    FileStorage fStorage(file, FileStorage::READ);
-
-    if (fStorage.isOpened())
-    {
-        fNode = fStorage.getFirstTopLevelNode();
-
-        if (fNode.type() == FileNode::SEQ)
-        {
-            for(FileNodeIterator iterator = fNode.begin(); iterator != fNode.end(); ++iterator)
-            {
-                list.push_back((string) *iterator);
-            }
-
-            loadSuccess = true;
-        }
-    }
-
-    return loadSuccess;
-}
-
-bool loadImages(vector<string> fileList, vector<Mat> &images)
-{
-    bool emptyImage = false;
-    for (int i = 0; i < fileList.size() && !emptyImage; ++i)
-    {
-        Mat curImg = imread(fileList[i]);
-        if (!curImg.empty())
-        {
-            images.push_back(curImg);
-        }
-        else
-        {
-            emptyImage = true;
-        }
-    }
-
-    return (!emptyImage && images.size() > 0);
-}
-
-bool loadQMatrix(string file, Mat &Q)
-{
-    bool success = false;
-    try
-    {
-        FileStorage fStorage(file.c_str(), FileStorage::READ);
-        fStorage["Q"] >> Q;
-        fStorage.release();
-        success = true;
-    }
-    catch(Exception ex)
-    {
-    }
-
-    return success;
-}
-
-void createAndSavePointCloud(Mat &disparity, Mat &leftImage, Mat &Q, string filename)
-{
-    pcl::PointCloud<pcl::PointXYZRGB> pointCloud;
-
-    // Read out Q Values for faster access
-    double Q03 = Q.at<double>(0, 3);
-    double Q13 = Q.at<double>(1, 3);
-    double Q23 = Q.at<double>(2, 3);
-    double Q32 = Q.at<double>(3, 2);
-    double Q33 = Q.at<double>(3, 3);
-
-    for (int i = 0; i < disparity.rows; i++)
-    {
-        for (int j = 0; j < disparity.cols; j++)
-        {
-            // Create a new point
-            pcl::PointXYZRGB point;
-
-            // Read disparity
-            float d = disparity.at<float>(i, j);
-            if ( d <= 0 ) continue; //Discard bad pixels
-
-            // Read color
-            Vec3b colorValue = leftImage.at<Vec3b>(i, j);
-            point.r = static_cast<int>(colorValue[2]);
-            point.g = static_cast<int>(colorValue[1]);
-            point.b = static_cast<int>(colorValue[0]);
-
-            // Transform 2D -> 3D and normalise to point
-            double x = Q03 + j;
-            double y = Q13 + i;
-            double z = Q23;
-            double w = (Q32 * d) + Q33;
-            point.x = -x / w;
-            point.y = -y / w;
-            point.z = z / w;
-
-            // Put point into the cloud
-            pointCloud.points.push_back (point);
-        }
-    }
-
-    // Resize PCL and save to file
-    pointCloud.width = pointCloud.points.size();
-    pointCloud.height = 1;
-    pcl::io::savePCDFileASCII(filename, pointCloud);
-}
-
 int main(int argc, char *argv[])
 {
     bool readSuccessfully = false;
-    libconfig::Config cfg;
-
     bool success = false;
 
-    string xmlImages, ymlExtrinsic;
     uint dMin; uint dMax; Size censusWin; float defaultBorderCost;
     float lambdaAD; float lambdaCensus; string savePath; uint aggregatingIterations;
     uint colorThreshold1; uint colorThreshold2; uint maxLength1; uint maxLength2; uint colorDifference;
     float pi1; float pi2; uint dispTolerance; uint votingThreshold; float votingRatioThreshold;
     uint maxSearchDepth; uint blurKernelSize; uint cannyThreshold1; uint cannyThreshold2; uint cannyKernelSize;
 
-    if(argc == 2)
-    {
+    dMin = 0;
+    dMax = 60;
+    censusWin.height = 9;
+    censusWin.width = 7;
+    defaultBorderCost = 0.999;
+    lambdaAD = 10.0; 
+    lambdaCensus = 30.0;
+    savePath = "../results/";
+    aggregatingIterations = 4;
+    colorThreshold1 = 20;
+    colorThreshold2 = 6;
+    maxLength1 = 34;
+    maxLength2 = 17;
+    colorDifference = 15;
+    pi1 = 0.1;
+    pi2 = 0.3;
+    dispTolerance = 0;
+    votingThreshold = 20;
+    votingRatioThreshold = 0.4;
+    maxSearchDepth = 20;
+    cannyThreshold1 = 20;
+    cannyThreshold2 = 60;
+    cannyKernelSize = 3;
+    blurKernelSize = 3;
 
-        try
+    bool error = false;
+    int frame_num = atoi(argv[1]);
+    for (int i = 0; i < frame_num && !error; ++i)
+    {
+        stringstream file;
+        file << savePath << i;
+        
+        std::clock_t start = std::clock();
+
+        ImageProcessor iP(0.1);
+        Mat eLeft, eRight, imleft, imright;
+        imleft = imread(argv[2], CV_LOAD_IMAGE_UNCHANGED);
+        imright = imread(argv[3], CV_LOAD_IMAGE_UNCHANGED);
+        if (imleft.rows > 0 && imright.rows > 0 )
         {
-            cfg.readFile(argv[1]);
             readSuccessfully = true;
         }
-        catch(const libconfig::FileIOException &fioex)
+        else
         {
-            cerr << "[ADCensusCV] I/O error while reading file." << endl;
+            std::cout << "Image " << i << "cannot be loaded" << std::endl;
+            continue; 
         }
-        catch(const libconfig::ParseException &pex)
+        eLeft = iP.unsharpMasking(imleft, "gauss", 3, 1.9, -1);
+        eRight = iP.unsharpMasking(imright, "gauss", 3, 1.9, -1);
+
+        StereoProcessor sP(dMin, dMax, imleft, imright, censusWin, defaultBorderCost, lambdaAD, lambdaCensus, file.str(),
+                            aggregatingIterations, colorThreshold1, colorThreshold2, maxLength1, maxLength2,
+                            colorDifference, pi1, pi2, dispTolerance, votingThreshold, votingRatioThreshold,
+                            maxSearchDepth, blurKernelSize, cannyThreshold1, cannyThreshold2, cannyKernelSize);
+        string errorMsg;
+        error = !sP.init(errorMsg);
+
+        if(!error && sP.compute())
         {
-            cerr << "[ADCensusCV] Parsing error" << endl;
+            success = true;
+            Mat disp = sP.getDisparity();
         }
-
-
-        if(readSuccessfully)
+        else
         {
-            try
-            {
-                dMin = (uint) cfg.lookup("dMin");
-                dMax = (uint) cfg.lookup("dMax");
-                xmlImages = (const char *) cfg.lookup("xmlImages");
-                ymlExtrinsic = (const char *) cfg.lookup("ymlExtrinsic");
-                censusWin.height = (uint) cfg.lookup("censusWinH");
-                censusWin.width = (uint) cfg.lookup("censusWinW");
-                defaultBorderCost = (float) cfg.lookup("defaultBorderCost");
-                lambdaAD = (float) cfg.lookup("lambdaAD"); // TODO Namen anpassen
-                lambdaCensus = (float) cfg.lookup("lambdaCensus");
-                savePath = (const char *) cfg.lookup("savePath");
-                aggregatingIterations = (uint) cfg.lookup("aggregatingIterations");
-                colorThreshold1 = (uint) cfg.lookup("colorThreshold1");
-                colorThreshold2 = (uint) cfg.lookup("colorThreshold2");
-                maxLength1 = (uint) cfg.lookup("maxLength1");
-                maxLength2 = (uint) cfg.lookup("maxLength2");
-                colorDifference = (uint) cfg.lookup("colorDifference");
-                pi1 = (float) cfg.lookup("pi1");
-                pi2 = (float) cfg.lookup("pi2");
-                dispTolerance = (uint) cfg.lookup("dispTolerance");
-                votingThreshold = (uint) cfg.lookup("votingThreshold");
-                votingRatioThreshold = (float) cfg.lookup("votingRatioThreshold");
-                maxSearchDepth = (uint) cfg.lookup("maxSearchDepth");
-                blurKernelSize = (uint) cfg.lookup("blurKernelSize");
-                cannyThreshold1 = (uint) cfg.lookup("cannyThreshold1");
-                cannyThreshold2 = (uint) cfg.lookup("cannyThreshold2");
-                cannyKernelSize = (uint) cfg.lookup("cannyKernelSize");
-            }
-            catch(const libconfig::SettingException &ex)
-            {
-                cerr << "[ADCensusCV] " << ex.what() << endl
-                     << "config file format:\n"
-                        "dMin(uint)\n"
-                        "xmlImages(string)\n"
-                        "ymlExtrinsic(string)\n"
-                        "censusWinH(uint)\n"
-                        "censusWinW(uint)\n"
-                        "defaultBorderCost(float)\n"
-                        "lambdaAD(float)\n"
-                        "lambdaCensus(float)\n"
-                        "savePath(string)\n"
-                        "aggregatingIterations(uint)\n"
-                        "colorThreshold1(uint)\n"
-                        "colorThreshold2(uint)\n"
-                        "maxLength1(uint)\n"
-                        "maxLength2(uint)\n"
-                        "colorDifference(uint)\n"
-                        "pi1(float)\n"
-                        "pi2(float)\n"
-                        "dispTolerance(uint)\n"
-                        "votingThreshold(uint)\n"
-                        "votingRatioThreshold(float)\n"
-                        "maxSearchDepth(uint)\n"
-                        "blurKernelSize(uint)\n"
-                        "cannyThreshold1(uint)\n"
-                        "cannyThreshold2(uint)\n"
-                        "cannyKernelSize(uint)\n";
-                readSuccessfully = false;
-            }
+            cerr << "[ADCensusCV] " << errorMsg << endl;
         }
-
-        if(readSuccessfully)
-        {
-            vector<string> fileList;
-            vector<Mat> images;
-            Mat Q(4, 4, CV_64F);
-
-            boost::filesystem::path dir(savePath);
-            boost::filesystem::create_directories(dir);
-
-            bool gotExtrinsic = loadQMatrix(ymlExtrinsic, Q);
-            if(loadImageList(xmlImages, fileList))
-            {
-                if(loadImages(fileList, images))
-                {
-                    if (images.size() % 2 == 0)
-                    {
-                        bool error = false;
-                        for (int i = 0; i < (images.size() / 2) && !error; ++i)
-                        {
-                            stringstream file;
-                            file << savePath << i;
-                            boost::posix_time::ptime start = boost::posix_time::second_clock::local_time();
-                            boost::posix_time::ptime end;
-                            boost::posix_time::time_duration diff;
-
-                            ImageProcessor iP(0.1);
-                            Mat eLeft, eRight;
-                            eLeft = iP.unsharpMasking(images[i * 2], "gauss", 3, 1.9, -1);
-                            eRight = iP.unsharpMasking(images[i * 2 + 1], "gauss", 3, 1.9, -1);
-
-                            StereoProcessor sP(dMin, dMax, images[i * 2], images[i * 2 + 1], censusWin, defaultBorderCost, lambdaAD, lambdaCensus, file.str(),
-                                               aggregatingIterations, colorThreshold1, colorThreshold2, maxLength1, maxLength2,
-                                               colorDifference, pi1, pi2, dispTolerance, votingThreshold, votingRatioThreshold,
-                                               maxSearchDepth, blurKernelSize, cannyThreshold1, cannyThreshold2, cannyKernelSize);
-                            string errorMsg;
-                            error = !sP.init(errorMsg);
-
-                            if(!error && sP.compute())
-                            {
-                                success = true;
-                                if(gotExtrinsic)
-                                {
-                                    Mat disp = sP.getDisparity();
-
-                                    string dispFile = file.str();
-                                    dispFile += "_disp.yml";
-                                    FileStorage fs(dispFile, FileStorage::WRITE);
-                                    fs << "disp" << disp;
-                                    fs.release();
-
-                                    file << "_cloud.pcd";
-                                    createAndSavePointCloud(disp, images[i * 2], Q, file.str());
-                                }
-                                else
-                                {
-                                    cerr << "[ADCensusCV] Could not create point cloud (no extrinsic)!" << endl;
-                                }
-                            }
-                            else
-                            {
-                                cerr << "[ADCensusCV] " << errorMsg << endl;
-                            }
-
-                            end = boost::posix_time::second_clock::local_time();
-
-                            diff = end - start;
-
-                            cout << "Finished computation after " << setw(2) << right <<  setfill('0') << ((int)(diff.total_seconds() / 3600)) << ":"
-                                                                   << setw(2) << right <<  setfill('0') << ((int)((diff.total_seconds() / 60) % 60)) << ":"
-                                                                   << setw(2) << right <<  setfill('0') << (diff.total_seconds() % 60) << " (" << diff.total_seconds() << "s) !" << endl;
-
-                        }
-                    }
-                    else
-                    {
-                        cerr << "[ADCensusCV] Not an even image number!" << endl;
-                    }
-                }
-                else
-                {
-                    cerr << "[ADCensusCV] Could not read images!" << endl;
-                }
-            }
-            else
-            {
-                cerr << "[ADCensusCV] Could not read image list!" << endl;
-            }
-        }
+        std::clock_t end = std::clock();
+        double diff = (end - start) / (double)CLOCKS_PER_SEC;;
+        cout << "Computation time: " << diff << "s" << endl;
     }
-    else
-    {
-        cerr << "[ADCensusCV] ADCensusBM <config file>" << endl;
-    }
-
-    return (success)? EXIT_SUCCESS: EXIT_FAILURE;
 }
 
